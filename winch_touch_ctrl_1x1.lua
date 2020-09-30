473d2f59449f3f9f09c343f202b7b2ba1be9f291
Nitroxleecher <nitroxleecher@gmail.com>
2020-09-30 22:34:00 +0200

    Update winch_touch_ctrl_1x1.lua

--- winch_touch_ctrl_1x1.lua
+++ winch_touch_ctrl_1x1.lua
@@ -11,8 +11,6 @@ by = 22
 bUpActive = false
 bDownActive = false
 
-bMoveFactor = 1/60
-
 positionWindow = 0.1
 
 function within(v,a,b)
@@ -28,13 +26,21 @@ function onTick()
     currLength = input.getNumber(7)
     maxLength = input.getNumber(8)
     targetLength = input.getNumber(9)
-	origTargetLength = targetLength
-	
-    positionWindow = input.getNumber(10)
-    if positionWindow == nil or positionWindow == 0 then
+	positionWindow = input.getNumber(10)
+	bMoveFactor = input.getNumber(11)
+
+	if positionWindow == nil or positionWindow == 0 then
         positionWindow = 0.1
     end
-    
+
+	if bMoveFactor == nil  or bMoveFactor == 0 then
+	    bMoveFactor = 1
+	end
+	
+	bMoveFactor = bMoveFactor / 60
+	
+	origTargetLength = targetLength
+	
     
     bDownActive = isPressed1 and within(input1X,b1x,b1x+btnSize) and within(input1Y,by,by+btnSize)
     bUpActive = isPressed1 and within(input1X,b2x,b2x+btnSize) and within(input1Y,by,by+btnSize)
@@ -66,19 +72,17 @@ function onDraw()
     w = screen.getWidth()
     h = screen.getHeight()
     
-    current = math.floor(currLength / maxLength * (h - 4) + 0.5)
-    target = math.floor(targetLength / maxLength * (h - 4) + 0.5)
+    current = math.floor(currLength / maxLength * (h - 3) + 0.5)
+    target = math.floor(targetLength / maxLength * (h - 3) + 0.5)
 
-
-
-    screen.setColor(255,255,255)
-    screen.drawRect(3,1,3,30)
-    
     screen.setColor(60,60,255)
-    screen.drawRectF(4,2,1,current)
+    screen.drawRectF(4,3,2,current)
+
+	screen.setColor(255,255,255)
+    screen.drawRect(3,2,3,29)
 
     screen.setColor(255,255,255)
-    screen.drawTriangleF(0,target-2 + 2,2,target + 2,0,target+2 + 2)
+    screen.drawTriangleF(1,target-2 + 3,3,target + 3,1,target+2 + 3)
     
     screen.drawTextBox(7, 2, 25, 7, tostring(math.floor((currLength*10))/10),1,0)
 
@@ -101,8 +105,18 @@ function onDraw()
     screen.drawRectF(b2x,by, btnSize,btnSize)
     
     screen.setColor(0,0,0)
-    screen.drawTriangleF(b1x+arrowMrgn,by+arrowMrgn,b1x+btnSize-arrowMrgn, by+arrowMrgn,b1x+btnSize/2,by+btnSize-arrowMrgn)
-    screen.drawTriangleF(b2x+arrowMrgn,by+btnSize-arrowMrgn,b2x+btnSize-arrowMrgn, by+btnSize-arrowMrgn,b2x+btnSize/2,by+arrowMrgn)
+	screen.drawLine(b1x+arrowMrgn, by+arrowMrgn, b1x+arrowMrgn, by+arrowMrgn+2)
+	screen.drawLine(b1x+arrowMrgn+1, by+arrowMrgn, b1x+arrowMrgn+1, by+arrowMrgn+4)
+	screen.drawLine(b1x+arrowMrgn+2, by+arrowMrgn, b1x+arrowMrgn+2, by+arrowMrgn+6)
+	screen.drawLine(b1x+arrowMrgn+3, by+arrowMrgn, b1x+arrowMrgn+3, by+arrowMrgn+4)
+	screen.drawLine(b1x+arrowMrgn+4, by+arrowMrgn, b1x+arrowMrgn+4, by+arrowMrgn+2)
+    
+
+	screen.drawLine(b2x+arrowMrgn, by+arrowMrgn+3, b2x+arrowMrgn, by+arrowMrgn+5)
+	screen.drawLine(b2x+arrowMrgn+1, by+arrowMrgn+1, b2x+arrowMrgn+1, by+arrowMrgn+5)
+	screen.drawLine(b2x+arrowMrgn+2, by+arrowMrgn-1, b2x+arrowMrgn+2, by+arrowMrgn+5)
+	screen.drawLine(b2x+arrowMrgn+3, by+arrowMrgn+1, b2x+arrowMrgn+3, by+arrowMrgn+5)
+	screen.drawLine(b2x+arrowMrgn+4, by+arrowMrgn+3, b2x+arrowMrgn+4, by+arrowMrgn+5)
 end
 
 function httpReply(port, request_body, response_body)
